d1242039aa842ddcfc77b0c931c709478a821d5e
Sowwtky <miche46ale@gmail.com>
2022-08-31 12:17:41 +0200

    add get mapping

--- src/main/java/jana60/Controller/LocationController.java
+++ src/main/java/jana60/Controller/LocationController.java
@@ -1,5 +1,23 @@
 package jana60.Controller;
 
-public class LocationController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jana60.Repository.LocationRepository;
+
+@Controller
+@RequestMapping("/location")
+public class LocationController {
+	
+	@Autowired
+	private LocationRepository locationRepo;
+	
+	@GetMapping
+	public String locationList(Model model) {
+		model.addAttribute("location", locationRepo.findAll());
+		return "/location/location";
+	}
 }
