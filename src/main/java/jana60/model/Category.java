a4747562a4259f7afdd5d22dfe2c7beb6784c041
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-09-01 06:44:47 +0200

    add import.sql, add list of categories

--- src/main/java/jana60/model/Category.java
+++ src/main/java/jana60/model/Category.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 @Table (name = "categories")
 public class Category 
 {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -22,4 +23,35 @@ public class Category
 	@Lob
 	private String description;
 	
+	
+	//GETTER & SETTER
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
+
+
+
+
