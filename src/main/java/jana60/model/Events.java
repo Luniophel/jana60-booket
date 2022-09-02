da066831472a6d32adb7036766de881a00f2e7d9
OlivieroPinto <pintooliviero@gmail.com>
2022-09-02 12:25:42 +0200

    add location and relation with event

--- src/main/java/jana60/model/Events.java
+++ src/main/java/jana60/model/Events.java
@@ -8,7 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -49,6 +51,10 @@ public class Events {
 	
 	@ManyToMany
 	List<Category> categories;
+	
+	@ManyToOne
+	@JoinColumn(name = "location_id")
+	private Location eventLocation;
 	
 
 	public LocalDate getStartDate() {
