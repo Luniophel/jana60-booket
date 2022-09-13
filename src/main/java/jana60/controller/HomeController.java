4d92bc27ee31cf502f9ed853a4b8f51de6cefc86
Sowwtky <miche46ale@gmail.com>
2022-09-13 17:25:59 +0200

    home

--- src/main/java/jana60/controller/HomeController.java
+++ src/main/java/jana60/controller/HomeController.java
@@ -1,18 +1,30 @@
 package jana60.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jana60.model.Events;
+import jana60.repository.EventsRepository;
 
 @Controller
 @RequestMapping("/")
 public class HomeController{
 	
+	
+	@Autowired
+	private EventsRepository repo;
+	
 	@GetMapping
-	public String home()
+	public String home(Model model)
 	{	
+		List<Events> listEvents = (List<Events>) repo.findAll();
+		model.addAttribute("listEvents", listEvents);
 		return "/home";
-		
 	}
 }
 	
